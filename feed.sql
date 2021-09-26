0824ba74a28dd9b2a50efbdd54a1e2623f9b6bd6
Jacty <saej3101@usherbrooke.ca>
2021-09-26 09:27:24 -0400

    Ajout des fichiers du projet

---
+++ feed.sql
@@ -0,0 +1,7 @@
+
+-- Insertions Table Campus --
+INSERT INTO Campus (nom, adresse) VALUES ('Campus de Longueuil', 'Adresse Longueuil');
+INSERT INTO Campus (nom, adresse) VALUES ('Campus de l''Ouest', 'Adresse Campus principale');
+INSERT INTO Campus (nom, adresse) VALUES ('Campus de l''Est', 'Adresse Campus de la santé');
+
+
---
+++ init.sql
@@ -0,0 +1,163 @@
+CREATE TABLE Campus
+(
+    id_campus SERIAL PRIMARY KEY NOT NULL,
+    nom VARCHAR(20) NOT NULL,
+    adresse VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE Pavillon
+(
+    id_pavillon CHAR(2) NOT NULL,
+    nom VARCHAR(20) NOT NULL,
+    id_campus INT NOT NULL,
+    PRIMARY KEY (id_pavillon),
+    FOREIGN KEY (id_campus) REFERENCES Campus(id_campus),
+    CONSTRAINT pavillon_id_pavillon
+        CHECK (id_pavillon SIMILAR TO '[A-Z]{1}[0-9]{1}')
+);
+
+CREATE TABLE Faculte
+(
+    id_faculte SERIAL PRIMARY KEY NOT NULL,
+    nom VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Statut
+(
+    id_statut SERIAL PRIMARY KEY NOT NULL,
+    nom VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Privilege
+(
+    id_privilege SERIAL PRIMARY KEY NOT NULL,
+    nom VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Local
+(
+    nom_local CHAR(4) NOT NULL,
+    capacite INT NOT NULL,
+    description VARCHAR(300),
+    id_parent CHAR(4),
+    id_pavillon CHAR(2) NOT NULL,
+    PRIMARY KEY (nom_local),
+    FOREIGN KEY (id_pavillon) REFERENCES Pavillon(id_pavillon),
+    CONSTRAINT local_id_local
+        CHECK (nom_local SIMILAR TO '[0-9]{4}')
+);
+
+CREATE TABLE Fonction
+(
+    id_fonction INT NOT NULL,
+    nom INT NOT NULL,
+    nom_local CHAR(4) NOT NULL,
+    PRIMARY KEY (id_fonction),
+    FOREIGN KEY (nom_local) REFERENCES Local(nom_local)
+);
+
+CREATE TABLE Operation
+(
+    id_operation SERIAL PRIMARY KEY NOT NULL,
+    nom INT NOT NULL
+);
+
+CREATE TABLE Pavillon_Faculte
+(
+    id_pavillon_faculte SERIAL PRIMARY KEY NOT NULL,
+    id_pavillon CHAR(2) NOT NULL,
+    id_faculte INT NOT NULL,
+    FOREIGN KEY (id_pavillon) REFERENCES Pavillon(id_pavillon),
+    FOREIGN KEY (id_faculte) REFERENCES Faculte(id_faculte)
+);
+
+CREATE TABLE Statut_Privilege
+(
+    id_statut_privilege SERIAL PRIMARY KEY NOT NULL,
+    id_statut INT NOT NULL,
+    id_privilege INT NOT NULL,
+    FOREIGN KEY (id_statut) REFERENCES Statut(id_statut),
+    FOREIGN KEY (id_privilege) REFERENCES Privilege(id_privilege)
+);
+
+CREATE TABLE Local_Privilege
+(
+    id_local_privilege SERIAL PRIMARY KEY NOT NULL,
+    nom_local CHAR(4) NOT NULL,
+    id_privilege INT NOT NULL,
+    FOREIGN KEY (nom_local) REFERENCES Local(nom_local),
+    FOREIGN KEY (id_privilege) REFERENCES Privilege(id_privilege)
+);
+
+CREATE TABLE Caracteristique
+(
+    id_caracteristique SERIAL PRIMARY KEY NOT NULL,
+    nom VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE Local_caracteristique
+(
+    id_local_caracteristiques SERIAL PRIMARY KEY NOT NULL,
+    nom_local CHAR(4) NOT NULL,
+    id_caracteristique INT NOT NULL,
+    FOREIGN KEY (nom_local) REFERENCES Local(nom_local),
+    FOREIGN KEY (id_caracteristique) REFERENCES Caracteristique(id_caracteristique)
+);
+
+
+
+CREATE TABLE Departement
+(
+    id_departement VARCHAR(10) NOT NULL,
+    nom INT NOT NULL,
+    id_faculte INT NOT NULL,
+    PRIMARY KEY (id_departement, id_faculte),
+    FOREIGN KEY (id_faculte) REFERENCES Faculte(id_faculte)
+);
+
+CREATE TABLE Membre
+(
+    cip CHAR(8) NOT NULL,
+    nom VARCHAR(20) NOT NULL,
+    prenom VARCHAR(20) NOT NULL,
+    courriel VARCHAR(30) NOT NULL,
+    num_telephone INT NOT NULL,
+    id_departement VARCHAR(10),
+    id_faculte INT,
+    PRIMARY KEY (cip),
+    FOREIGN KEY (id_departement, id_faculte) REFERENCES Departement(id_departement, id_faculte),
+    CONSTRAINT membre_cip
+        CHECK (cip SIMILAR TO '[A-Z]{4}[0-9]{4}')
+);
+
+CREATE TABLE Reservation
+(
+    debut INT NOT NULL,
+    fin INT NOT NULL,
+    id_reservation SERIAL PRIMARY KEY NOT NULL,
+    date DATE NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    etat BOOLEAN NOT NULL,
+    nom_local CHAR(4) NOT NULL,
+    cip CHAR(8) NOT NULL,
+    FOREIGN KEY (nom_local) REFERENCES Local(nom_local),
+    FOREIGN KEY (cip) REFERENCES Membre(cip)
+);
+
+CREATE TABLE Role
+(
+    cip CHAR(8) NOT NULL,
+    id_statut INT NOT NULL,
+    PRIMARY KEY (cip, id_statut),
+    FOREIGN KEY (cip) REFERENCES Membre(cip),
+    FOREIGN KEY (id_statut) REFERENCES Statut(id_statut)
+);
+
+CREATE TABLE Journal
+(
+    cip CHAR(8) NOT NULL,
+    id_operation INT NOT NULL,
+    PRIMARY KEY (cip, id_operation),
+    FOREIGN KEY (cip) REFERENCES Membre(cip),
+    FOREIGN KEY (id_operation) REFERENCES Operation(id_operation)
+);
