32e0d3a68fd8bb268f5f75a44de4f8d7baf136fe
Jacty <saej3101@usherbrooke.ca>
2021-09-26 13:40:33 -0400

    ÈInsertion

--- sql/feed.sql
+++ sql/feed.sql
@@ -10,8 +10,8 @@ INSERT INTO Pavillon (id_pavillon, nom, id_campus) VALUES ('C1', 'J.-Armand-Bomb
 INSERT INTO Pavillon (id_pavillon, nom, id_campus) VALUES ('D7', 'Marie-Victorin', 2);
 
 -- Insertions Table Faculte --
-INSERT INTO Faculte (nom) VALUES ('Faculté de génie');
-INSERT INTO Faculte (nom) VALUES ('Faculté des sciences');
+INSERT INTO Faculte (nom) VALUES ('Génie');
+INSERT INTO Faculte (nom) VALUES ('Sciences');
 
 -- Insertions Table Pavillon_Faculte --
 INSERT INTO pavillon_faculte (id_pavillon, id_faculte) VALUES ('C1', 1);
@@ -71,6 +71,70 @@ INSERT INTO Fonction (id_fonction, nom) VALUES ('0930', 'Salon du personnel');
 INSERT INTO Fonction (id_fonction, nom) VALUES ('1030', 'Studio d''enregistrement');
 INSERT INTO Fonction (id_fonction, nom) VALUES ('1260', 'Hall d''entrée');
 
+-- Insertions Table Local --
+INSERT INTO Local (nom_local, capacite, id_pavillon, id_fonction, description)
+    VALUES ('1007', 21, 'C1', '0212', 'Grand');
+INSERT INTO Local (nom_local, capacite, id_pavillon,id_fonction, description)
+    VALUES ('2028', 10, 'C1', '0212', 'Matériaux composites');
+INSERT INTO Local (nom_local, capacite, id_pavillon,id_fonction)
+    VALUES ('2055', 24, 'C1', '0211');
+INSERT INTO Local (nom_local, capacite, id_pavillon,id_fonction, description)
+    VALUES ('3014', 25, 'C1', '0211', 'Laboratoire mécatronique');
+INSERT INTO Local (nom_local, capacite, id_pavillon,id_fonction, description)
+    VALUES ('3027', 15, 'C1', '0211', 'Petit laboratoire de élec');
+INSERT INTO Local (nom_local, capacite, id_pavillon,id_fonction, description)
+VALUES ('3007', 106, 'C1', '0620', 'Avec console multi-média');
+
+-- Insertions Table Local_privilege --
+INSERT INTO local_privilege (nom_local, id_privilege) VALUES ('1007', 1);
+INSERT INTO local_privilege (nom_local, id_privilege) VALUES ('3027', 1);
+INSERT INTO local_privilege (nom_local, id_privilege) VALUES ('3014', 1);
+INSERT INTO local_privilege (nom_local, id_privilege) VALUES ('3007', 2);
+INSERT INTO local_privilege (nom_local, id_privilege) VALUES ('2055', 2);
+INSERT INTO local_privilege (nom_local, id_privilege) VALUES ('2028', 2);
+
+-- Insertions Table Caracteristique --
+INSERT INTO Caracteristique (nom) VALUES ('Connexion à Internet');
+INSERT INTO Caracteristique (nom) VALUES ('Tables fixes en U et chaises mobiles');
+INSERT INTO Caracteristique (nom) VALUES ('Monoplaces');
+INSERT INTO Caracteristique (nom) VALUES ('Tables fixes et chaises fixes');
+INSERT INTO Caracteristique (nom) VALUES ('Tables pour 2 ou + et chaises mobiles');
+INSERT INTO Caracteristique (nom) VALUES ('Tables mobiles et chaises mobiles');
+INSERT INTO Caracteristique (nom) VALUES ('Tables hautes et chaises hautes');
+INSERT INTO Caracteristique (nom) VALUES ('Tables fixes et chaises mobiles');
+INSERT INTO Caracteristique (nom) VALUES ('Écran');
+INSERT INTO Caracteristique (nom) VALUES ('Rétroprojecteur');
+INSERT INTO Caracteristique (nom) VALUES ('Gradins');
+INSERT INTO Caracteristique (nom) VALUES ('Fenêtres');
+INSERT INTO Caracteristique (nom) VALUES ('1 piano');
+INSERT INTO Caracteristique (nom) VALUES ('2 pianos');
+INSERT INTO Caracteristique (nom) VALUES ('Autres instruments');
+INSERT INTO Caracteristique (nom) VALUES ('Système de son');
+INSERT INTO Caracteristique (nom) VALUES ('Salle réservée (spéciale)');
+INSERT INTO Caracteristique (nom) VALUES ('Ordinateurs PC');
+INSERT INTO Caracteristique (nom) VALUES ('Ordinateurs SUN pour génie électrique');
+INSERT INTO Caracteristique (nom) VALUES ('Ordinateurs (oscillomètre et multimètre)');
+INSERT INTO Caracteristique (nom) VALUES ('Ordinateurs modélisation des structures');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement pour microélectronique');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement pour génie électrique');
+INSERT INTO Caracteristique (nom) VALUES ('Ordinateurs et équipement pour mécatroni');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement métrologie');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement de machinerie');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement de géologie');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement pour la caractérisation');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement pour la thermodynamique');
+INSERT INTO Caracteristique (nom) VALUES ('Équipement pour génie civil');
+INSERT INTO Caracteristique (nom) VALUES ('Télévision');
+INSERT INTO Caracteristique (nom) VALUES ('VHS');
+INSERT INTO Caracteristique (nom) VALUES ('Hauts parleurs');
+INSERT INTO Caracteristique (nom) VALUES ('Micro');
+INSERT INTO Caracteristique (nom) VALUES ('Magnétophone à cassette');
+INSERT INTO Caracteristique (nom) VALUES ('Amplificateur audio');
+INSERT INTO Caracteristique (nom) VALUES ('Local barré');
+INSERT INTO Caracteristique (nom) VALUES ('Prise réseau');
+
+
+
 
 SELECT * FROM Faculte;
 SELECT * FROM Pavillon;
@@ -78,3 +142,4 @@ SELECT * FROM pavillon_faculte;
 SELECT * FROM Departement;
 SELECT * FROM statut_privilege;
 SELECT * FROM privilege;
+SELECT * FROM Local;
